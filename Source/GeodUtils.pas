b3599a49812a03e76bd3f693e51c98fe92ac3123
Paul Michell <paul@michellcomputing.co.uk>
2015-10-12 15:05:55 +0100

    Extend the default decimal places for seconds of arc to 4.
    Signed-off-by: Paul Michell <paul@michellcomputing.co.uk>

--- Source/GeodUtils.pas
+++ Source/GeodUtils.pas
@@ -33,7 +33,7 @@ Type
 
 Const
   SexigesimalDelims = [#0..' '];
-  SexagesimalFormat = '%.0F° %.0F'' %.3F" ';
+  SexagesimalFormat = '%.0F° %.0F'' %.4F" ';
   CompactSexagesimalFormat = '%.0FD%.0FM%.3FS';
 
 Function FormatCoordinate(Const Coordinate: TSexagesimalCoordinate; Option: TSexagesimalOption = soPlusMinusPrefix; FormatPattern: String = SexagesimalFormat): String; Overload;
