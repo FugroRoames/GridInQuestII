f5164e90b32f99f08b6e3f5a0ed7027f7c78d687
Paul Michell <paul@michellcomputing.co.uk>
2015-10-12 20:44:10 +0100

    Completed TN02/GM02 reverse transform routine.
    Signed-off-by: Paul Michell <paul@michellcomputing.co.uk>

--- Source/BNG.pas
+++ Source/BNG.pas
@@ -22,7 +22,7 @@ Unit BNG;
 Interface
 
 Uses
-  Math, Geometry, Geodesy;
+  SysUtils, Math, Geometry, Geodesy;
 
 { Define OS TNO2 polynomial accuracy. }
 {$DEFINE OSTNO2RES_1KM}  { WARNING: This option will add 20Mb data table to the executable! }
@@ -176,56 +176,77 @@ Begin
 End;
 
 Function BNGCoordinatesToWGS84Coordinates(Const Coordinates: TCoordinates; DatumModel: TVerticalDatumModel = OSVRF10): TCoordinates;
+{$IFDEF OSTNO2TABLEUSED}
 Var
   EastOffset, NorthOffset, GeoidHeight: TCoordinate;
+  PriorResult: TCoordinates;
+  Iteration: Integer;
+Const
+  IterationLimit = 6;
+  Epsilon: TCoordinate = 0.0001;
+{$ENDIF}
 Begin
   Result := Coordinates;
-  {$IFDEF OSTNO2RES_1KM}
-    { Perform full kilometer resolution polynomial correction. }
-    { This is the intrinsic conversion as defined by TN02 but requires a 20Mb data table. }
-    BilinearInterpolate(Result, 1000, EastOffset, NorthOffset, GeoidHeight);
-    With Result Do
-      Begin
-        Easting := Easting-EastOffset;
-        Northing := Northing-NorthOffset;
-        Elevation := Elevation+GeoidHeight;
-      End;
-  {$ENDIF}
-  {$IFDEF OSTNO2RES_10KM}
-    { Perform 10 kilometer resolution polynomial correction. }
-    { Max Easting Error: 0.371 Max Northing Error: 0.375 Max Geoid Height Error: 0.554 }
-    BilinearInterpolate(Result, 10000, EastOffset, NorthOffset, GeoidHeight);
-    With Result Do
-      Begin
-        Easting := Easting-EastOffset;
-        Northing := Northing-NorthOffset;
-        Elevation := Elevation+GeoidHeight;
-      End;
-  {$ENDIF}
-  {$IFDEF OSTNO2RES_100KM}
-    { Perform 100 kilometer resolution polynomial correction. }
-    { Max Easting Error: 2.790 Max Northing Error: 2.784 Max Geoid Height Error: 2.389 }
-    BilinearInterpolate(Result, 100000, EastOffset, NorthOffset, GeoidHeight);
-    With Result Do
-      Begin
-        Easting := Easting-EastOffset;
-        Northing := Northing-NorthOffset;
-        Elevation := Elevation+GeoidHeight;
-      End;
-  {$ENDIF}
-  {$IFNDEF OSTNO2TABLEUSED}
-    { Instead of a polynomial correction, apply mean deltas which yeild a standard deviation of less than 10m in all directions. }
-    {           Easting Shift  Northing Shift  Geoid Height  }
-    { Min.          86.27         -81.60          43.98      }
-    { Mean          96.04         -68.87          51.16      }
-    { Max.         103.44         -50.16          57.66      }
-    { St. Dev.       3.29           9.45           3.47      }
-    With Result Do
-      Begin
-        Easting := Easting-96.04;
-        Northing := Northing+68.87;
-        Elevation := Elevation+51.16; { Geoid height is subtracted. }
-      End;
+  {$IFDEF OSTNO2TABLEUSED}
+  PriorResult := NullCoordinates;
+  For Iteration := 1 To IterationLimit Do
+    Begin
+      {$IFDEF OSTNO2RES_1KM}
+        { Perform full kilometer resolution polynomial correction. }
+        { This is the intrinsic conversion as defined by TN02 but requires a 20Mb data table. }
+        BilinearInterpolate(Result, 1000, EastOffset, NorthOffset, GeoidHeight);
+        With Result Do
+          Begin
+            Easting := Coordinates.Easting-EastOffset;
+            Northing := Coordinates.Northing-NorthOffset;
+            Elevation := Coordinates.Elevation+GeoidHeight;
+          End;
+      {$ENDIF}
+      {$IFDEF OSTNO2RES_10KM}
+        { Perform 10 kilometer resolution polynomial correction. }
+        { Max Easting Error: 0.371 Max Northing Error: 0.375 Max Geoid Height Error: 0.554 }
+        BilinearInterpolate(Result, 10000, EastOffset, NorthOffset, GeoidHeight);
+        With Result Do
+          Begin
+            Easting := Coordinates.Easting-EastOffset;
+            Northing := Coordinates.Northing-NorthOffset;
+            Elevation := Coordinates.Elevation+GeoidHeight;
+          End;
+      {$ENDIF}
+      {$IFDEF OSTNO2RES_100KM}
+        { Perform 100 kilometer resolution polynomial correction. }
+        { Max Easting Error: 2.790 Max Northing Error: 2.784 Max Geoid Height Error: 2.389 }
+        BilinearInterpolate(Result, 100000, EastOffset, NorthOffset, GeoidHeight);
+        With Result Do
+          Begin
+            Easting := Coordinates.Easting-EastOffset;
+            Northing := Coordinates.Northing-NorthOffset;
+            Elevation := Coordinates.Elevation+GeoidHeight;
+          End;
+      {$ENDIF}
+      { If convergance has been reached. }
+      If (Abs(Result.Easting-PriorResult.Easting)<Epsilon) And
+         (Abs(Result.Northing-PriorResult.Northing)<Epsilon) Then
+         Begin
+           Result := InverseTransverseMercator(Result, OSTN02GridProjection);
+           Exit;
+         End;
+      PriorResult := Result;
+    End;
+  Raise Exception.Create('Inverse BNG projection failed to converge grid to ETRS89 coordinates.');
+  {$ELSE}
+  { Instead of a polynomial correction, apply mean deltas which yeild a standard deviation of less than 10m in all directions. }
+  {           Easting Shift  Northing Shift  Geoid Height  }
+  { Min.          86.27         -81.60          43.98      }
+  { Mean          96.04         -68.87          51.16      }
+  { Max.         103.44         -50.16          57.66      }
+  { St. Dev.       3.29           9.45           3.47      }
+  With Result Do
+    Begin
+      Easting := Easting-96.04;
+      Northing := Northing+68.87;
+      Elevation := Elevation+51.16; { Geoid height is subtracted. }
+    End;
   {$ENDIF}
 End;
 
@@ -233,7 +254,7 @@ Function TBNGCoordinateSystem02.ConvertToGeocentric(Coordinates: TCoordinates): 
 Var
   GeodeticCoordinates: TCoordinates;
 Begin
-  GeodeticCoordinates := BNGCoordinatesToWGS84Coordinates(GeodeticCoordinates);
+  GeodeticCoordinates := BNGCoordinatesToWGS84Coordinates(Coordinates);
   Result := GeodeticToGeocentric(GeodeticCoordinates, GRS80Ellipsoid);
 End;
 
