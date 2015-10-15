c59f81ca1f2305833e86d1a0d7fe391bc37531f3
Paul Michell <paul@michellcomputing.co.uk>
2015-10-15 19:15:23 +0100

    Initial implementation of ITM without height transform.
    Signed-off-by: Paul Michell <paul@michellcomputing.co.uk>

--- Source/ITM.pas
+++ Source/ITM.pas
@@ -30,22 +30,32 @@ Type TITMCoordinateSystem95 = Object(TCoordinateSystem)
   End;
 
 Var
+  GRS80Ellipsoid: TEllipsoid;
+  ITMProjection: TProjection;
   ITMCoordinateSystem95: TITMCoordinateSystem95;
 
 Implementation
 
 Function TITMCoordinateSystem95.ConvertToGeocentric(Coordinates: TCoordinates): TCoordinates;
+Var
+  GeodeticCoordinates: TCoordinates;
 Begin
-  Result := NullCoordinates;
+  GeodeticCoordinates := InverseTransverseMercator(Coordinates, ITMProjection);
+  Result := GeodeticToGeocentric(GeodeticCoordinates, GRS80Ellipsoid);
 End;
 
 Function TITMCoordinateSystem95.ConvertFromGeocentric(Coordinates: TCoordinates): TCoordinates;
+Var
+  GeodeticCoordinates: TCoordinates;
 Begin
-  Result := NullCoordinates;
+  GeodeticCoordinates := GeocentricToGeodetic(Coordinates, GRS80Ellipsoid);
+  Result := TransverseMercator(GeodeticCoordinates, ITMProjection);
 End;
 
 Initialization
 
+GRS80Ellipsoid.Initialize(6378137.0000, 6356752.314140);
+ITMProjection.Initialize(0.99982, DegToRad(53.5), DegToRad(-8), 600000, 750000, GRS80Ellipsoid);
 ITMCoordinateSystem95.Initialize('Irish Transverse Mercator', 'IRENET95',
                                  'IRENET95 / Irish Transverse Mercator (ITM/VRF10)',
                                   2157, ctCartesian, aoXYZ);
