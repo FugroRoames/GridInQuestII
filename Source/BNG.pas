0d98bae9ace881a60a012b84dde1ca4cf89691bb
Paul Michell <paul@michellcomputing.co.uk>
2015-10-19 02:58:03 +0100

    Moved BilinearInterpolate to OSMod unit.
    Signed-off-by: Paul Michell <paul@michellcomputing.co.uk>

--- Source/BNG.pas
+++ Source/BNG.pas
@@ -45,55 +45,6 @@ Function BNGCoordinatesToWGS84Coordinates(Const Coordinates: TCoordinates; Datum
 
 Implementation
 
-{$IFDEF OSTNO2TABLEUSED}
-Procedure BilinearInterpolate(Const Coordinates: TCoordinates; Const GridScale: TCoordinate; Var EastOffset, NorthOffset, GeoidHeight: TCoordinate);
-Var
-  InvGridScale: TCoordinate;
-  X1, X2, Y1, Y2: Integer;
-  SE0, SE1, SE2, SE3: TCoordinate;
-  SN0, SN1, SN2, SN3: TCoordinate;
-  SG0, SG1, SG2, SG3: TCoordinate;
-  T, TI, U, UI: TCoordinate;
-Begin
-  InvGridScale := 1/GridScale;
-  X1 := Trunc(Coordinates.Easting*InvGridScale);
-  Y1 := Trunc(Coordinates.Northing*InvGridScale);
-  X2 := X1+1;
-  Y2 := Y1+1;
-  With OSTN02Data[X1, Y1] Do
-    Begin
-      SE0 := Easting;
-      SN0 := Northing;
-      SG0 := Elevation;
-    End;
-  With OSTN02Data[X2, Y1] Do
-    Begin
-      SE1 := Easting;
-      SN1 := Northing;
-      SG1 := Elevation;
-    End;
-  With OSTN02Data[X2, Y2] Do
-    Begin
-      SE2 := Easting;
-      SN2 := Northing;
-      SG2 := Elevation;
-    End;
-  With OSTN02Data[X1, Y2] Do
-    Begin
-      SE3 := Easting;
-      SN3 := Northing;
-      SG3 := Elevation;
-    End;
-  T := (Coordinates.Easting-(X1*GridScale))*InvGridScale;
-  TI := (1-T);
-  U := (Coordinates.Northing-(Y1*GridScale))*InvGridScale;
-  UI := (1-U);
-  EastOffset := (TI*UI*SE0)+(T*UI*SE1)+(T*U*SE2)+(TI*U*SE3);
-  NorthOffset := (TI*UI*SN0)+(T*UI*SN1)+(T*U*SN2)+(TI*U*SN3);
-  GeoidHeight := (TI*UI*SG0)+(T*UI*SG1)+(T*U*SG2)+(TI*U*SG3);
-End;
-{$ENDIF}
-
 Function WGS84CoordinatesToBNGCoordinates(Const Coordinates: TCoordinates; DatumModel: TVerticalDatumModel = OSVRF10): TCoordinates;
 Var
   EastOffset, NorthOffset, GeoidHeight: TCoordinate;
@@ -260,7 +211,6 @@ End;
 
 Initialization
 
-//Airy1830Ellipsoid.Initialize(6377563.3960, 6356256.9100);
 GRS80Ellipsoid.Initialize(6378137.0000, 6356752.314140);
 OSTN02GridProjection.Initialize(0.9996012717, DegToRad(49), DegToRad(-2), 400000, -100000, GRS80Ellipsoid);
 BNGCoordinateSystem02.Initialize('British National Grid (2002)', 'OSGB36',
