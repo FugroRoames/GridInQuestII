06b4f0057387a1a72766f50577dab326396ac388
Paul Michell <Paul@MichellComputing.co.uk>
2016-06-09 12:37:31 +0100

    Update to python example to allow for Linux/OSX execution and support additional revision parameters.
    Signed-off-by: Paul Michell <Paul@MichellComputing.co.uk>

--- Examples/Python/GridInQuestIIExample.py
+++ Examples/Python/GridInQuestIIExample.py
@@ -13,13 +13,34 @@
 # for more details.
 
 # Setup required library imports.
+import sys
 import os
 from math import radians as deg2rad
 from ctypes import *
 
+
 # Load the GIQ dynamic library.
-SourceFolder = os.path.dirname(__file__)
-giqdll = WinDLL(os.path.join(SourceFolder, "GIQ.dll"))
+if sys.platform.startswith('win'):
+  LibName = "GIQ.dll"
+elif sys.platform.startswith('linux'):
+  LibName = "libgiq.so"
+elif sys.platform.startswith('darwin'):
+  LibName = "libGIQ.dylib"
+
+# Construct the GIQ library file path.
+LibFolder = os.path.split(os.path.realpath(__file__))[0]
+LibPath = os.path.join(LibFolder, LibName)
+
+# Load the GIQ dynamic library.
+if sys.platform.startswith('win'):
+  GIQLib = WinDLL(LibPath)
+elif sys.platform.startswith('linux'):
+  GIQLib = cdll.LoadLibrary(LibPath)
+elif sys.platform.startswith('darwin'):
+  GIQLib = cdll.LoadLibrary(LibPath)
+else:
+  print("Unsupported OS")
+  exit()
 
 # Define the library coordinate structure.
 class coordinates(Structure):
@@ -28,28 +49,31 @@ class coordinates(Structure):
                  ("z", c_double)]
 
 # Reference the library convert function.
-convert = giqdll.ConvertCoordinates
-convert.argtypes = [c_int, c_int, POINTER(coordinates),
+Convert = GIQLib.ConvertCoordinates
+Convert.argtypes = [c_int, c_int, c_int, c_int, 
+                    POINTER(coordinates),
                     POINTER(coordinates), POINTER(c_int)]
-convert.restype = bool
+Convert.restype = bool
 
 # Setup the calling parameter values.
-SRIDsource = c_int(4937) # ETRS89 Geodetic
-SRIDtarget = c_int(2157) # Irish Transverse Mercator (ITM/GM15)
-source = coordinates(deg2rad(-7), deg2rad(53), 100) # Longitude, Latitude, Altitude
-target = coordinates(0, 0, 0)
-datum = c_int(13) # Malin Head datum
-callok = bool(False)
+SRIDSource = c_int(4937) # ETRS89 Geodetic.
+RevisionSource = c_int(0) # No revision required.
+SRIDTarget = c_int(2157) # Irish Transverse Mercator. 
+RevisionTarget = c_int(2015) # Revision for ITM/GM15.  This can also be 2002 for ITM/GM02.
+Source = coordinates(deg2rad(-7), deg2rad(53), 100) # Longitude, Latitude, Altitude.
+Target = coordinates(0, 0, 0)
+Datum = c_int(13) # Malin Head datum.
+CallOK = bool(False)
 
 # Call coordinate converter.
-callok = convert(SRIDsource, SRIDtarget, source, target, datum)
+CallOK = Convert(SRIDSource, SRIDTarget, RevisionSource, RevisionTarget, Source, Target, Datum)
 
 # Output the result.
-if callok:
+if CallOK:
   print("Conversion result...")
-  print("ITM Easting: "+str(target.x))
-  print("ITM Northing: "+str(target.y))
-  print("Elevation: "+str(target.z))
+  print("ITM Easting: "+str(Target.x))
+  print("ITM Northing: "+str(Target.y))
+  print("Elevation: "+str(Target.z))
   Datums = {
             0: "None",
             1: "Ordnance Datum Newlyn",
@@ -68,6 +92,6 @@ if callok:
             14: "Belfast",
             15: "Offshore",
            }
-  print("Vertical Datum: "+Datums[datum.value])
+  print("Vertical Datum: "+Datums[Datum.value])
 else:
   print("Error converting coordinates")
